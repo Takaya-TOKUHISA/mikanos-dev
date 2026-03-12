2caeff27d6c711cf975364009b849930b050a18f
Takaya-TOKUHISA <nanimon401@gmail.com>
2026-03-12 18:58:23 +0900

    day12c

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -30,6 +30,7 @@
 #include "layer.hpp"
 #include "timer.hpp"
 #include "acpi.hpp"
+#include "keyboard.hpp"
 
 #include "message.hpp"
 
@@ -93,8 +94,7 @@ extern "C" void KernelMainNewStack(const FrameBufferConfig& frame_buffer_config_
     acpi::Initialize(acpi_table);
     InitializeLAPICTimer(*main_queue);
 
-    timer_manager->AddTimer(Timer(200, 2));
-    timer_manager->AddTimer(Timer(600, -1));
+    InitializeKeyboard(*main_queue);
 
     char str[128];
 
@@ -123,11 +123,10 @@ extern "C" void KernelMainNewStack(const FrameBufferConfig& frame_buffer_config_
             usb::xhci::ProcessEvents();
                 break;
             case Message::kTimerTimeout:
-                printk("Timer: timeout = %lu, value = %d\n",
-                    msg.arg.timer.timeout, msg.arg.timer.value);
-                if (msg.arg.timer.value > 0) {
-                    timer_manager->AddTimer(Timer(
-                        msg.arg.timer.timeout + 100, msg.arg.timer.value + 1));
+                break;
+            case Message::kKeyPush:
+                if (msg.arg.keyboard.ascii != 0) {
+                    printk("%c", msg.arg.keyboard.ascii);
                 }
                 break;
             default:
