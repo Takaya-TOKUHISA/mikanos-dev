ed45859c6c58818096b87ede20d6533c3f146e87
Takaya-TOKUHISA <nanimon401@gmail.com>
2026-03-14 18:45:28 +0900

    day14b

--- kernel/interrupt.cpp
+++ kernel/interrupt.cpp
@@ -9,6 +9,7 @@
 #include "asmfunc.h"
 #include "segment.hpp"
 #include "timer.hpp"
+#include "task.hpp"
 
 std::array<InterruptDescriptor, 256> idt;
 
@@ -31,11 +32,10 @@ void NotifyEndOfInterrupt() {
 }
 
 namespace {
-    std::deque<Message>* msg_queue;
 
     __attribute__((interrupt))
     void IntHandlerXHCI(InterruptFrame* frame) {
-        msg_queue->push_back(Message{Message::kInterruptXHCI});
+        task_manager->SendMessage(1, Message{Message::kInterruptXHCI});
         NotifyEndOfInterrupt();
     }
 
@@ -45,9 +45,7 @@ namespace {
     }
 }
 
-void InitializeInterrupt(std::deque<Message>* msg_queue) {
-    ::msg_queue = msg_queue;
-
+void InitializeInterrupt(){
     SetIDTEntry(idt[InterruptVector::kXHCI],
                 MakeIDTAttr(DescriptorType::kInterruptGate, 0),
                 reinterpret_cast<uint64_t>(IntHandlerXHCI),
